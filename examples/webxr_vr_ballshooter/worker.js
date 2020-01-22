22806b878650a21831439cfa6987f8ace9cf0893
Surma <surma@surma.link>
2020-01-22 00:50:01 +0000

    Switch to per-ball objects

--- examples/webxr_vr_ballshooter/worker.js
+++ examples/webxr_vr_ballshooter/worker.js
@@ -35,6 +35,13 @@ class BallShooter {
 		this.framerate = 90;
 		this._positions = new Float32Array(this._pool.get());
 		this._velocities = new Float32Array(this._pool.get());
+		this._balls = Array.from({ length: this._numBalls }, (_, i) => {
+			return {
+				index: i,
+				position: this._positions.subarray(i * 3, i * 3 + 3),
+				velocity: this._velocities.subarray(i * 3, i * 3 + 3)
+			};
+		});
 		this._init();
 	}
 
@@ -44,19 +51,19 @@ class BallShooter {
 
 	_init() {
 		for (var i = 0; i < this._numBalls; i++) {
-			this._positions[i * 3 + 0] = random(
+			this._balls[i].position[0] = random(
 				-this._roomSize / 2 + 1,
 				this._roomSize / 2 - 1
 			);
-			this._positions[i * 3 + 1] = random(0, this._roomSize);
-			this._positions[i * 3 + 2] = random(
+			this._balls[i].position[1] = random(0, this._roomSize);
+			this._balls[i].position[2] = random(
 				-this._roomSize / 2 + 1,
 				this._roomSize / 2 - 1
 			);
 
-			this._velocities[i * 3 + 0] = random(-0.005, 0.005);
-			this._velocities[i * 3 + 1] = random(-0.005, 0.005);
-			this._velocities[i * 3 + 2] = random(-0.005, 0.005);
+			this._balls[i].velocity[0] = random(-0.005, 0.005);
+			this._balls[i].velocity[1] = random(-0.005, 0.005);
+			this._balls[i].velocity[2] = random(-0.005, 0.005);
 		}
 	}
 
@@ -99,77 +106,64 @@ class BallShooter {
 		const range = this._roomSize / 2 - this._radius;
 		const normal = new Float32Array(3);
 		const relativeVelocity = new Float32Array(3);
-		for (var i = 0; i < this._numBalls * 3; i += 3) {
-			this._positions[i + 0] += this._velocities[i + 0] * delta;
-			this._positions[i + 1] += this._velocities[i + 1] * delta;
-			this._positions[i + 2] += this._velocities[i + 2] * delta;
+		for (var i = 0; i < this._numBalls; i++) {
+			const ball = this._balls[i];
+			ball.position[0] += ball.velocity[0] * delta;
+			ball.position[1] += ball.velocity[1] * delta;
+			ball.position[2] += ball.velocity[2] * delta;
 
 			// Bounce of walls
-			if (this._positions[i + 0] < -range || this._positions[i + 0] > range) {
-				this._positions[i + 0] = clamp(this._positions[i + 0], -range, range);
-				this._velocities[i + 0] = -this._velocities[i + 0] * this._dampening;
+			if (ball.position[0] < -range || ball.position[0] > range) {
+				ball.position[0] = clamp(ball.position[0], -range, range);
+				ball.velocity[0] = -ball.velocity[0] * this._dampening;
 			}
 
 			if (
-				this._positions[i + 1] < this._radius ||
-				this._positions[i + 1] > this._roomSize
+				ball.position[1] < this._radius ||
+				ball.position[1] > this._roomSize
 			) {
-				this._positions[i + 1] = Math.max(this._positions[i + 1], this._radius);
+				ball.position[1] = Math.max(ball.position[1], this._radius);
 
-				this._velocities[i + 0] *= this._dampening;
-				this._velocities[i + 1] = -this._velocities[i + 1] * this._dampening;
-				this._velocities[i + 2] *= this._dampening;
+				ball.velocity[0] *= this._dampening;
+				ball.velocity[1] = -ball.velocity[1] * this._dampening;
+				ball.velocity[2] *= this._dampening;
 			}
 
-			if (this._positions[i + 2] < -range || this._positions[i + 2] > range) {
-				this._positions[i + 2] = clamp(this._positions[i + 2], -range, range);
-				this._velocities[i + 2] = -this._velocities[i + 2] * this._dampening;
+			if (ball.position[2] < -range || ball.position[2] > range) {
+				ball.position[2] = clamp(ball.position[2], -range, range);
+				ball.velocity[2] = -ball.velocity[2] * this._dampening;
 			}
 
 			// // Bounce of other balls
-			for (var j = i + 3; j < this._numBalls * 3; j += 3) {
-				vectorDifference(normal, 0, this._positions, i, this._positions, j);
+			for (var j = i + 1; j < this._numBalls; j++) {
+				const otherBall = this._balls[j];
+				vectorDifference(normal, ball.position, otherBall.position);
 
 				const distance = vectorLength(normal, 0);
 
 				if (distance < 2 * this._radius) {
-					vectorScalarProduct(
-						normal,
-						0,
-						normal,
-						0,
-						0.5 * distance - this._radius
-					);
+					vectorScalarProduct(normal, normal, 0.5 * distance - this._radius);
 
-					vectorDifference(this._positions, i, this._positions, i, normal, 0);
-					vectorSum(this._positions, j, this._positions, j, normal, 0);
+					vectorDifference(ball.position, ball.position, normal);
+					vectorSum(otherBall.position, otherBall.position, normal);
 
-					vectorNormalized(normal, 0, normal, 0);
+					vectorNormalized(normal, normal);
 
-					vectorDifference(
-						relativeVelocity,
-						0,
-						this._velocities,
-						i,
-						this._velocities,
-						j
-					);
+					vectorDifference(relativeVelocity, ball.velocity, otherBall.velocity);
 
 					vectorScalarProduct(
 						normal,
-						0,
 						normal,
-						0,
-						vectorDot(relativeVelocity, 0, normal, 0)
+						vectorDot(relativeVelocity, normal)
 					);
 
-					vectorDifference(this._velocities, i, this._velocities, i, normal, 0);
-					vectorSum(this._velocities, j, this._velocities, j, normal, 0);
+					vectorDifference(ball.velocity, ball.velocity, normal);
+					vectorSum(otherBall.velocity, otherBall.velocity, normal);
 				}
 			}
 
 			// Gravity
-			this._velocities[i + 1] -= 9.8 * delta;
+			ball.velocity[1] -= 9.8 * delta;
 		}
 	}
 }
@@ -184,41 +178,41 @@ function clamp(v, min, max) {
 	return v;
 }
 
-function vectorDot(a, oa, b, ob) {
-	return a[oa + 0] * b[ob + 0] + a[oa + 1] * b[ob + 1] + a[oa + 2] * b[ob + 2];
+function vectorDot(a, b) {
+	return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
 }
 
-function vectorSum(t, ot, a, oa, b, ob) {
+function vectorSum(t, a, b) {
 	for (let i = 0; i < 3; i++) {
-		t[ot + i] = a[oa + i] + b[ob + i];
+		t[i] = a[i] + b[i];
 	}
 	return t;
 }
 
-function vectorDifference(t, ot, a, oa, b, ob) {
+function vectorDifference(t, a, b) {
 	for (let i = 0; i < 3; i++) {
-		t[ot + i] = a[oa + i] - b[ob + i];
+		t[i] = a[i] - b[i];
 	}
 	return t;
 }
 
-function vectorLength(a, oa) {
-	let length = vectorDot(a, oa, a, oa);
+function vectorLength(a) {
+	let length = vectorDot(a, a);
 	length = Math.sqrt(length);
 	return length;
 }
 
-function vectorScalarProduct(t, ot, a, oa, s) {
+function vectorScalarProduct(t, a, s) {
 	for (let i = 0; i < 3; i++) {
-		t[ot + i] = a[oa + i] * s;
+		t[i] = a[i] * s;
 	}
 	return t;
 }
 
-function vectorNormalized(t, ot, a, oa) {
-	const length = vectorLength(a, oa);
+function vectorNormalized(t, a) {
+	const length = vectorLength(a);
 	for (let i = 0; i < 3; i++) {
-		t[ot + i] = a[oa + i] / length;
+		t[i] = a[i] / length;
 	}
 	return t;
 }
